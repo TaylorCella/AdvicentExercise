323c8868fbec16b02225b11ff96f9740ea826bf2
Taylor Cella <taylor@sitequesttech.com>
2020-12-18 22:18:43 -0700

    Finishes autocomplete, WIP get other values

--- advicent-exercise/src/app/college/college.component.ts
+++ advicent-exercise/src/app/college/college.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http/';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+import { Observable, fromEvent } from 'rxjs';
+import {map, startWith, debounceTime, distinctUntilChanged, tap} from 'rxjs/operators';
 
 export class college {
   collegeName: string;
@@ -21,14 +24,22 @@ export class college {
 })
 export class CollegeComponent implements OnInit {
   colleges: college[] = [];
+  @ViewChild('input', { static: false }) input: ElementRef;
+  filteredOptions: Observable<college[]>;
+  collegeForm: FormGroup = this.formBuilder.group({
+    collegeSelection: '',
+    inTuition: '',
+    outTuition: '',
+    roomBoard: ''
+  });
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private formBuilder: FormBuilder){
     this.http.get('assets/college_costs.csv', {responseType: 'text'})
     .subscribe(
         data => {
             let csvToRowArray = data.split("\n");
             for (let index = 1; index < csvToRowArray.length - 1; index++) {
-              let row = csvToRowArray[index].split(",");
+              let row = csvToRowArray[index].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
               this.colleges.push(new college(row[0].trim(), parseInt(row[1]), parseInt(row[2]), parseInt(row[3])));
             }
         },
@@ -38,7 +49,27 @@ export class CollegeComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.filteredOptions = this.collegeForm.get('collegeSelection')!.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filterGroup(value))
+      );
+  }
+
+  private _filterGroup(value: string): college[] {
+    if (value) {
+      var test = this.colleges.filter(option => option.collegeName.toLowerCase().includes(value));
+      console.log(test.values);
+      console.log(test[1]);
+      return test;
+    }
+    return this.colleges;
+  }
+
+  getCostValues(value){
+    let test = this._filterGroup(value);
+    this.collegeForm.get('inTuition').setValue(test[1]);
   }
 
 }
--- advicent-exercise/src/app/shared/shared.module.ts
+++ advicent-exercise/src/app/shared/shared.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatCardModule} from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -14,13 +15,21 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     MatAutocompleteModule,
     MatCardModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     CommonModule,
     MatAutocompleteModule,
     MatCardModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
